3145e7248c517ba2d8a5246827d55fceeef06904
Ponejalesk <ponejalesk@gmail.com>
2023-02-08 11:15:18 -0300

    2.0
    add js

---
+++ js/scriptcounterstd.js
@@ -0,0 +1,28 @@
+let valorveneno = 0;
+
+document.getElementById('bmenos').onclick = function(){
+    valorveneno-=1;
+    document.getElementById('valorveneno').innerText = valorveneno;
+}
+document.getElementById('bmais').onclick = function(){
+    valorveneno+=1;
+    document.getElementById('valorveneno').innerText = valorveneno;
+}
+
+let stdvalorvida = 20;
+
+document.getElementById('stdvmenos').onclick = function(){
+    stdvalorvida-=1;
+    document.getElementById('stdvalorvida').innerText = stdvalorvida;
+}
+document.getElementById('stdvmais').onclick = function(){
+    stdvalorvida+=1;
+    document.getElementById('stdvalorvida').innerText = stdvalorvida;
+}
+
+document.getElementById('reset').onclick = function(){
+    stdvalorvida=20;
+    document.getElementById('stdvalorvida').innerText = stdvalorvida;
+    valorveneno=0;
+    document.getElementById('valorveneno').innerText = valorveneno;
+}
