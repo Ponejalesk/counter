7f2f33e3028c4a961ea9c24ec23c56ce9cceb278
Ponejalesk <ponejalesk@gmail.com>
2023-04-02 13:00:38 -0300

    3.0

--- js/scriptcounter.js
+++ js/scriptcounter.js
@@ -1,54 +1,71 @@
 let valorcastcmd = 0;
-
-document.getElementById('btnmenos').onclick = function(){
-    valorcastcmd-=2;
-    document.getElementById('valorcastcmd').innerText = valorcastcmd;
+//Valor do marcador Cast Comandante não pode ficar abaixo de 0.
+document.getElementById('btnmenos').onclick = function () {
+    if (valorcastcmd !== 0) {
+        valorcastcmd -= 2;
+        document.getElementById('valorcastcmd').innerText = valorcastcmd;
+    }
 }
-document.getElementById('btnmais').onclick = function(){
-    valorcastcmd+=2;
+document.getElementById('btnmais').onclick = function () {
+    valorcastcmd += 2;
     document.getElementById('valorcastcmd').innerText = valorcastcmd;
 }
 
 let valordanocmd = 0;
-
-document.getElementById('btmenos').onclick = function(){
-    valordanocmd-=1;
-    document.getElementById('valordanocmd').innerText = valordanocmd;
+//Valor do marcador Dano de Comandante não pode ficar abaixo de 0.
+document.getElementById('btmenos').onclick = function () {
+    if (valordanocmd !== 0) {
+        valordanocmd -= 1;
+        document.getElementById('valordanocmd').innerText = valordanocmd;
+    }
 }
-document.getElementById('btmais').onclick = function(){
-    valordanocmd+=1;
+//Se o valor do marcador Dano Comandante for igual ou maior que 21, vence o jogo.
+document.getElementById('btmais').onclick = function () {
+    valordanocmd += 1;
     document.getElementById('valordanocmd').innerText = valordanocmd;
+    if(valordanocmd >= 21) {
+        alert("Parabéns você venceu o jogo!");
+    }
 }
 
 let valorveneno = 0;
 
-document.getElementById('bmenos').onclick = function(){
-    valorveneno-=1;
-    document.getElementById('valorveneno').innerText = valorveneno;
+document.getElementById('bmenos').onclick = function () {
+    if (valorveneno !== 0) {
+        valorveneno -= 1;
+        document.getElementById('valorveneno').innerText = valorveneno;
+    }
 }
-document.getElementById('bmais').onclick = function(){
-    valorveneno+=1;
+document.getElementById('bmais').onclick = function () {
+    valorveneno += 1;
     document.getElementById('valorveneno').innerText = valorveneno;
 }
 
 let valorvida = 40;
-
-document.getElementById('vmenos').onclick = function(){
-    valorvida-=1;
+//Se a vida ficar abixo de 5 deve mudar a cor da fonte para vermelho.
+document.getElementById('vmenos').onclick = function () {
+    valorvida -= 1;
     document.getElementById('valorvida').innerText = valorvida;
+    if (valorvida <= 5) {
+        document.querySelector('.valorvida').classList.add('danger');
+    }
 }
-document.getElementById('vmais').onclick = function(){
-    valorvida+=1;
+document.getElementById('vmais').onclick = function () {
+    valorvida += 1;
     document.getElementById('valorvida').innerText = valorvida;
+    if(valorvida >= 6) {
+        document.querySelector('.valorvida').classList.remove('danger');
+    }
 }
-
-document.getElementById('reset').onclick = function(){
-    valorcastcmd=0;
+//Resetar para os valors padrões.
+document.getElementById('reset').onclick = function () {
+    valorcastcmd = 0;
     document.getElementById('valorcastcmd').innerText = valorcastcmd;
-    valordanocmd=0;
+    valordanocmd = 0;
     document.getElementById('valordanocmd').innerText = valordanocmd;
-    valorveneno=0;
+    valorveneno = 0;
     document.getElementById('valorveneno').innerText = valorveneno;
-    valorvida=40;
+    valorvida = 40;
     document.getElementById('valorvida').innerText = valorvida;
+    document.querySelector('.valorvida').classList.remove('danger');
 }
--- js/scriptcounterstd.js
+++ js/scriptcounterstd.js
@@ -1,28 +1,37 @@
 let valorveneno = 0;
-
-document.getElementById('bmenos').onclick = function(){
-    valorveneno-=1;
-    document.getElementById('valorveneno').innerText = valorveneno;
+//Valor do marcador veneno não pode ficar abaixo de 0.
+document.getElementById('bmenos').onclick = function () {
+    if (valorveneno !== 0) {
+        valorveneno -= 1;
+        document.getElementById('valorveneno').innerText = valorveneno;
+    }
 }
-document.getElementById('bmais').onclick = function(){
-    valorveneno+=1;
+document.getElementById('bmais').onclick = function () {
+    valorveneno += 1;
     document.getElementById('valorveneno').innerText = valorveneno;
 }
 
 let stdvalorvida = 20;
-
-document.getElementById('stdvmenos').onclick = function(){
-    stdvalorvida-=1;
+//Se a vida ficar abixo de 5 deve mudar a cor da fonte para vermelho.
+document.getElementById('stdvmenos').onclick = function () {
+    stdvalorvida -= 1;
     document.getElementById('stdvalorvida').innerText = stdvalorvida;
+    if (stdvalorvida <= 5) {
+        document.querySelector('.valorvida').classList.add('danger');
+    }
 }
-document.getElementById('stdvmais').onclick = function(){
-    stdvalorvida+=1;
+document.getElementById('stdvmais').onclick = function () {
+    stdvalorvida += 1;
     document.getElementById('stdvalorvida').innerText = stdvalorvida;
+    if(stdvalorvida >= 6) {
+        document.querySelector('.valorvida').classList.remove('danger');
+    }
 }
-
-document.getElementById('reset').onclick = function(){
-    stdvalorvida=20;
+//Resetar para os valores padrões.
+document.getElementById('reset').onclick = function () {
+    stdvalorvida = 20;
     document.getElementById('stdvalorvida').innerText = stdvalorvida;
-    valorveneno=0;
+    valorveneno = 0;
     document.getElementById('valorveneno').innerText = valorveneno;
+    document.querySelector('.valorvida').classList.remove('danger');
 }
